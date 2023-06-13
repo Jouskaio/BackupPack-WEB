3469bd439e42ab3c44169023bbca6e22e9c35988
Jouskaio <jouskaio.me@gmail.com>
2023-06-13 21:18:59 +0200

    Add itinary

--- public/script/widget_map.js
+++ public/script/widget_map.js
@@ -40,6 +40,25 @@ $(document).ready(function() {
     });
   }
 
+  // Calculate and Display Route
+  function displayMapAndRoute() {
+    initMap();
+    calculateAndDisplayRoute();
+  }
+  function calculateAndDisplayRoute() {
+    let request = {
+      origin: user,
+      destination: bag,
+      travelMode: google.maps.TravelMode.DRIVING // ou google.maps.TravelMode.WALKING pour la marche
+    };
+
+    directionsService.route(request, function(response, status) {
+      if (status === google.maps.DirectionsStatus.OK) {
+        directionsDisplay.setDirections(response);
+        directionsDisplay.setPanel(document.querySelector(".l-location__a-directionPannel")); // Afficher les instructions d'itinéraire dans un élément HTML
+      }
+    });
+  }
   // Update Map
   function updateMap(newCoordinates) {
     if (map && marker) {
@@ -52,6 +71,8 @@ $(document).ready(function() {
   // Display Map
   window.initMap = initMap;
   google.maps.event.addDomListener(window, 'load', initMap);
+  displayMapAndRoute();
+
 
   // Example usage: update map with new coordinates
   //let newCoordinates = { latitude: 48.8566, longitude: 2.3522 };
